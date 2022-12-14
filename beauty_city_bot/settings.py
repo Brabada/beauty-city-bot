dbe3be107aae3542e696b79c0bfd0656fcce78b7
Brabada <freemindobject@gmail.com>
2022-12-14 20:10:30 +0300

    Add models Client, Visit, Master, Service, Saloon, Day, WorkingTime

--- beauty_city_bot/settings.py
+++ beauty_city_bot/settings.py
@@ -44,6 +44,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'beauty_saloon_db.apps.BeautySaloonDbConfig',
+    "phonenumber_field"
 ]
 
 MIDDLEWARE = [
--- beauty_saloon_db/admin.py
+++ beauty_saloon_db/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 
+from .models import Client, Visit, Master, Service, Saloon, Day, WorkingTime
 # Register your models here.
 
-admin.re
+admin.site.register(Client)
+admin.site.register(Visit)
+admin.site.register(Master)
+admin.site.register(Service)
+admin.site.register(Saloon)
+admin.site.register(Day)
+admin.site.register(WorkingTime)
