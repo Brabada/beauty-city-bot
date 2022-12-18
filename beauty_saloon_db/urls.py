4d2c363e1ff96a2dce3072a9ba8b77512cf1c3b4
Brabada <freemindobject@gmail.com>
2022-12-19 02:35:58 +0300

    Added API get all services, get seven days, get working times of saloon for day

--- beauty_saloon_db/urls.py
+++ beauty_saloon_db/urls.py
@@ -1,9 +1,20 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from beauty_saloon_db.views import SaloonsListView
+from beauty_saloon_db.views import (
+    SaloonsListView,
+    SaloonDetailView,
+    ServicesListView,
+    DaysListView,
+    DayListMastersView,
+)
 
 app_name='beauty_saloon_db'
 urlpatterns = [
-    path('saloon/all/', SaloonsListView.as_view())
+    path('saloon/all/', SaloonsListView.as_view()),
+    path('saloon/<int:pk>/', SaloonDetailView.as_view()),
+    path('service/all/', ServicesListView.as_view()),
+    path('day/week/', DaysListView.as_view()),
+    path('day/<int:year>/<int:month>/<int:day>/<str:saloon>/masters/',
+         DayListMastersView.as_view()),
 ]
--- beauty_saloon_db/views.py
+++ beauty_saloon_db/views.py
@@ -1,11 +1,52 @@
 from django.shortcuts import render
+from django.db import models
 from rest_framework import generics
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-from beauty_saloon_db.serializers import SaloonsListSerializer
-from beauty_saloon_db.models import Saloon
+import datetime
+
+from beauty_saloon_db.serializers import (
+    SaloonsListSerializer,
+    SaloonDetailSerializer,
+    ServiceSerializer,
+    DaysListSerializer,
+    DayListMastersSerializer,
+)
+
+from beauty_saloon_db.models import Saloon, Service, Day
 
 # Create your views here.
 class SaloonsListView(generics.ListAPIView):
     serializer_class = SaloonsListSerializer
-
     queryset = Saloon.objects.all()
+
+
+class SaloonDetailView(generics.RetrieveAPIView):
+    serializer_class = SaloonDetailSerializer
+    queryset = Saloon.objects.all()
+
+
+class ServicesListView(generics.ListAPIView):
+    serializer_class = ServiceSerializer
+    queryset = Service.objects.all()
+
+
+class DaysListView(generics.ListAPIView):
+    serializer_class = DaysListSerializer
+    queryset = Day.objects.filter(date__gte=datetime.date.today()).exclude(
+        date__gt=datetime.date.today() + datetime.timedelta(days=8)
+    ).values("date").annotate(n=models.Count("pk"))
+
+
+class DayListMastersView(APIView):
+    def get(self, request, year, month, day, saloon):
+        date = datetime.date(year, month, day)
+        days = Day.objects.filter(date__exact=date, saloon__name__exact=saloon)
+        # days = Day.objects.filter(date__exact=date,
+        # saloon__name__exact=saloon) а так не выводит
+
+        print(days)
+        serializer = DayListMastersSerializer(days, many=True)
+        return Response(serializer.data)
+
